6e4b8a70e62aa824de7ea9967f72624653b83b5f
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-08-01 10:42:30 +0530

    Update aws-config.js

--- Vikram Backend/aws-config.js
+++ Vikram Backend/aws-config.js
@@ -1,6 +1,6 @@
 // src/aws-config.js
 export const AWSConfig = {
-  accessKeyId: "AKIAZQ3DTBTRWELFEIW3",
-  secretAccessKey: "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",
-  region: "us-east-1",
+  accessKeyId: "",
+  secretAccessKey: "",
+  region: "",
 };
