c281cc917e76b54a325222958c70957716a34887
Kedarinadh Sai Harsha Gadu <119718764+SaiHarsha9992@users.noreply.github.com>
2024-08-01 10:43:34 +0530

    Update aws-config.js

--- face-rec/src/aws-config.js
+++ face-rec/src/aws-config.js
@@ -2,9 +2,9 @@
 import AWS from "aws-sdk";
 
 AWS.config.update({
-  region: "us-west-2",
-  accessKeyId: "AKIAZI2LIV5VCL4IGU3W",
-  secretAccessKey: "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",
+  region: "",
+  accessKeyId: "",
+  secretAccessKey: "",
 });
 
 export const s3 = new AWS.S3();
